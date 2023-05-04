52b534b220cb0f248e3b040ef002c97d87c71c15
Param Jagani <96529601+param9898@users.noreply.github.com>
2023-05-04 20:06:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+let name = "Vedi Heal";
+function App() {
+  return (
+    <>
+
+   <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+   </nav>
+   <h1>Hello {name}</h1>
+   <div className="container">
+   <p>
+   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed fugiat facilis odio iure earum quisquam inventore corrupti distinctio, cupiditate, dolorum delectus adipisci nostrum! Odio dolores exercitationem beatae, ducimus quo voluptas.</p> 
+   </div>
+   </>
+  );
+}
+
+export default App;
