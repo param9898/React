c45069c5c08bbc1718f5155283cec470d7e79139
Param Jagani <96529601+param9898@users.noreply.github.com>
2023-05-05 15:24:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+
+let name = "Vedi Heal";
+function App() {
+  return (
+   <>
+   <Navbar title = "TextUtils"/>
+   </>
+  );
+}
+
+export default App;
