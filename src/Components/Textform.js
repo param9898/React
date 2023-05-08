a55808ad99499aa7bc3df0e22e7437fa492e6ad1
Param Jagani <96529601+param9898@users.noreply.github.com>
2023-05-08 19:03:04 +0530

    Add files via upload

---
+++ src/Components/Textform.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Textform() {
+  return (
+            <div>
+        <div class="mb-3">
+    <label for="my-box" class="form-label">Example textarea</label>
+        <textarea class="form-control" id="my-box" rows="3"></textarea>
+        </div>
+            </div>
+  )
+}
